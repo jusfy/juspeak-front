b486345016d9021883886a9aad97bd606f900f17
Alexandre R. Saccol <arsaccol@gmail.com>
2023-10-11 16:25:19 -0300

    chore(api): add path for API URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //const API_URL = 'https://juspeak.mrprompt.com.br/api/v1';
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5734/api/v1';
+const API_URL = `${import.meta.env.VITE_API_URL}/api/v1` || 'http://localhost:5734/api/v1';
 
 export const postTranscription = (formData) =>
   axios.post(`${API_URL}/transcription`, formData);
