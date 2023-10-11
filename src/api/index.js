e0d3a06c9a29ea6db90a68e69f6bb3ee1f5806f3
Alexandre R. Saccol <arsaccol@gmail.com>
2023-10-11 15:21:06 -0300

    chore(api): get API_URL from an environment variable if it is present

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://juspeak.mrprompt.com.br/api/v1';
+//const API_URL = 'https://juspeak.mrprompt.com.br/api/v1';
+const API_URL = process.env.API_URL || 'http://localhost:5734/api/v1';
 
 export const postTranscription = (formData) =>
   axios.post(`${API_URL}/transcription`, formData);
